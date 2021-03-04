d6b4c9b680cbcd86769ed2779cad13c993e6f6a5
an288705 <67563101+an288705@users.noreply.github.com>
2021-03-03 20:47:21 -0500

    Add files via upload

---
+++ back-end/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+mongoose.connect(process.env.DB_URL,{useNewUrlParser : true, useUnifiedTopology : true},(err)=>{
+    if(!err) return console.log('connected to DB')
+
+    console.log(err);
+})
+
+/*set up express server*/
+const PORT = process.env.PORT || 3500
+
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
+app.use(cors());
+
+app.use('/auth',require('./Routes/Auth'));
+
+app.listen(PORT,()=>{
+    console.log('listening on '+PORT);
+});
